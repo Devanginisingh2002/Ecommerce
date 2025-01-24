692fa19d31e3ee281d9eb30cb80c047e57f1ad36
Devangini Singh <84927553+Devanginisingh2002@users.noreply.github.com>
2025-01-24 16:51:14 +0000

    file

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,10 +4,10 @@ const auth = (req,res,next) => {
     try{
         const token = req.header("Authorization")
         console.log(token)
-        if(!token) return res.status(400).json({msg:"Invalid Authentication"})
+        if(!token) return res.status(404).json({msg:"Invalid Authentication"})
 
         jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,user)=>{
-            if(err) return res.status(400).json({msg:"Invalid Authentication"})
+            if(err) return res.status(404).json({msg:"Invalid Authentication"})
 
             req.user = user
             next()
